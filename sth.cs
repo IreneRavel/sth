13ce33a28e84eb11955168d4ad2cc506846f5881
IreneRavel <93340020+IreneRavel@users.noreply.github.com>
2023-09-16 16:57:02 +0300

    Add files via upload

---
+++ sth.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sth : MonoBehaviour
+
+{
+    public int health = 10;
+
+    public int level = 5;
+
+    public float speed = 1.2f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health += level;
+
+        print("Player's health:" + health);
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get the current position of the NPC
+        Vector3 newPosition = transform.position;
+
+        // Update the position along the Z-axis based on speed and time
+        newPosition.z += speed * Time.deltaTime;
+
+        // Update the NPC's position
+        transform.position = newPosition;
+
+    }
+}
